6e17114da78f5edd3e45486d4be60e30e0d0e548
HashtagAssist <53903379+HashtagAssist@users.noreply.github.com>
2021-09-16 11:52:35 +0200

    Update Migration1567588155StorePlugins.php

--- src/Migration/Migration1567588155StorePlugins.php
+++ src/Migration/Migration1567588155StorePlugins.php
@@ -14,7 +14,7 @@ class Migration1567588155StorePlugins extends MigrationStep
 
     public function update(Connection $connection): void
     {
-        $connection->executeQuery(
+        $connection->executeUpdate(
             'CREATE TABLE IF NOT EXISTS `store_plugins`
             (
                 id BINARY(16) NOT NULL,
